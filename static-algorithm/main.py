db420189269d7b5614b4316fba9d5fd8a6df2fdf
Adrian Borup <adrian@borup.com>
2024-10-24 11:41:08 +0200

    Construct min-cost flow instance from max flow

--- static-algorithm/main.py
+++ static-algorithm/main.py
@@ -1,17 +1,34 @@
 from typing import Tuple
 import numpy as np
+from dataclasses import dataclass
 
 
-class Graph:
+@dataclass
+class MinCostFlow:
     m: int
     n: int
     edges: list[Tuple[int, int]]
+    c: np.ndarray
+    u_upper: np.ndarray
+    u_lower: np.ndarray
+    optimal_cost: int
     B: np.ndarray
 
-    def __init__(self, edges: list[Tuple[int, int]]) -> None:
+    def __init__(
+        self,
+        edges: list[Tuple[int, int]],
+        c: np.ndarray,
+        u_lower: np.ndarray,
+        u_upper: np.ndarray,
+        optimal_cost: int
+    ) -> None:
         self.m = len(edges)
         self.n = len(set(v for e in edges for v in e)) + 1
         self.edges = edges
+        self.c = c
+        self.u_lower = u_lower
+        self.u_upper = u_upper
+        self.optimal_cost = optimal_cost
 
         self.B = np.zeros((self.m, self.n), dtype=int)
         for e, (a, b) in enumerate(edges):
@@ -19,9 +36,41 @@ class Graph:
             self.B[e, b] = -1
 
     def print_B(self):
-        print(f"{'vertices:': >10} ", ' '.join([f"{i: >2}" for i in range(self.n)]))
+        print(f"{'vertices:': >10} ", ' '.join(
+            [f"{i: >2}" for i in range(self.n)]))
         for e, row in enumerate(self.B):
             print(f"{e: <2} {str(self.edges[e]): <7}", row)
+
+    @staticmethod
+    def from_max_flow_instance(edges: list[Tuple[int, int]], s: int, t: int, optimal_flow: int, capacities: list[int]):
+        new_edges = edges + [(s, t)]
+
+        c = np.array([0] * len(new_edges) + [-1])
+        u_lower = np.zeros(len(new_edges), dtype=int)
+        u_upper = np.array(capacities + [sum(capacities)])
+
+        I = MinCostFlow(edges=new_edges, u_lower=u_lower,
+                        u_upper=u_upper, c=c, optimal_cost=-optimal_flow)
+
+        return I
+
+    def phi(self, f: np.ndarray) -> float:
+        objective = ...
+        barrier = ...
+        return objective + barrier
+
+    def calc_gradients(self, f: np.ndarray) -> np.ndarray:
+        ...
+
+    def calc_lengths(self, f: np.ndarray) -> np.ndarray:
+        ...
+
+
+def max_flow(edges: list[Tuple[int, int]], capacities: list[int], s: int, t: int, optimal_flow: int):
+    I = MinCostFlow.from_max_flow_instance(
+        edges, s, t, optimal_flow, capacities)
+
+    print(I)
 
 
 if __name__ == "__main__":
@@ -36,7 +85,6 @@ if __name__ == "__main__":
         (5, 6),
     ]
 
-    G = Graph(edges)
+    capacities = [3, 2, 2, 1, 1, 1, 2, 3]
 
-    print(G.m, G.n)
-    G.print_B()
+    max_flow(edges, capacities, s=1, t=6, optimal_flow=3)
