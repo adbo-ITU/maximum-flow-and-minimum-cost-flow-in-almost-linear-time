b6213f1a87029d3e7235c9b8260de9ce7eedb4db
Adrian Borup <adrian@borup.com>
2024-10-30 14:56:11 +0100

    Add another test graph

--- static-algorithm/test_results.py
+++ static-algorithm/test_results.py
@@ -1,4 +1,5 @@
-from main import max_flow
+from main import max_flow, max_flow_with_guess
+import numpy as np
 
 
 def make_edges_and_capacities(graph):
@@ -42,3 +43,22 @@ def test_flow_sample_from_geeksforgeeks():
     edges, capacities = make_edges_and_capacities(graph)
 
     assert max_flow(edges, capacities, s=0, t=5) == 23
+
+
+def test_idk():
+    graph = [
+        ((0, 1), 13),
+        ((0, 2), 16),
+        ((1, 2), 4),
+        ((1, 3), 14),
+        ((2, 4), 12),
+        ((4, 1), 9),
+        ((3, 4), 7),
+        ((3, 5), 4),
+        ((4, 5), 20),
+    ]
+
+    edges, capacities = make_edges_and_capacities(graph)
+    # init_flow = np.array([6, 7, 1, 6, 8, 1, 3, 3, 10, 13], dtype=float)
+
+    assert max_flow_with_guess(edges, capacities, s=0, t=5, optimal_flow=23) == 23
